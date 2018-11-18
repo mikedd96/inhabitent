5cebe59923cc520197e9982151d1ca90891bb33b
mike <mike_4596@hotmail.com>
2018-11-17 22:32:34 -0800

    finished shop page

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -9,23 +9,46 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+        
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+                ?>
+                <div class="cat-tags">
+            <?php
+        // get term for products and do stuff with images.
+        $terms = get_terms(array(
+            'taxonomy' => 'product_type', 'hide_empty' => 0,
+        ));
+
+        foreach($terms as $term): ?>
+        
+        <div class="tags-a">
+        <a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a></p>
+        </div>
+    
+    
+        <?php endforeach;
+        ?>
+        </div>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
+                <div class="product-container">
+                <?php
                     get_template_part( 'template-parts/content' );
-                    the_product_title()
-				?>
+                ?>
+<div class="archive-text">
+   <?php the_title(); ?>
+   <?php get_template_part('price');?>
+   <?php echo CFS()->get( 'price' ); ?>
+    </div>
+</div>
 
 			<?php endwhile; ?>
 
--- themes/inhabitent/js/custom.js
+++ themes/inhabitent/js/custom.js
@@ -3,7 +3,15 @@
         $(document).ready(function(){
 
     //your code here
-            
+    $('.icon-search').on('click', function() {
+        $('.search-field').addClass('search-slide');
+        $('.search-field').focus();
+
+    }); 
+            $('.search-field').on('blur', function(){
+                $('.search-field').removeClass('search-slide');
+                
+            })
     });
 
 })(jQuery);
