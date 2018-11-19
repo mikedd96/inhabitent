0c4398f42113181a3f7e3a6bc98915ac4ad738b4
mike <mike_4596@hotmail.com>
2018-11-18 20:35:09 -0800

    added categories and tags

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -10,22 +10,47 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+        
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+					get_template_part( 'template-parts/content' );
+                ?>
+                <div class="cat-tags">
+            <?php
+        // get term for products and do stuff with images.
+        $terms = get_terms(array(
+            'taxonomy' => 'product_type', 'hide_empty' => 0,
+        ));
+
+        foreach($terms as $term): ?>
+        
+        <div class="tags-a">
+        <a href="<?php echo get_term_link( $term ); ?>"><?php echo $term->name; ?></a></p>
+        </div>
+    
+    
+        <?php endforeach;
+        ?>
+        </div>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+                <div class="product-container">
+                <?php
+                    get_template_part( 'template-parts/content' );
+                ?>
+<div class="archive-text">
+   <?php the_title(); ?>
+   <?php get_template_part('price');?>
+   <?php echo CFS()->get( 'price' ); ?>
+    </div>
+</div>
 
 			<?php endwhile; ?>
 
@@ -33,7 +58,8 @@ get_header(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+            
 
 		<?php endif; ?>
 
--- themes/inhabitent/template-parts/content.php
+++ themes/inhabitent/template-parts/content.php
@@ -9,9 +9,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<a href="<?php echo get_permalink( $post->ID); ?>">
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
+</a>
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
